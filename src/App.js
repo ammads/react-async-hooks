0a8a04818f5988345b881d7bcdcca54f1d4ded56
ammad.sikander@vass.es <ammad.sikander@vass.es>
2020-04-24 01:08:31 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const giphyKey = process.env.REACT_APP_GIPHY_KEY;
+
+function useGiphy(query) {
+
+  const [results, setRestults] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData ()  {
+      try {
+        setLoading(true);
+        const res = await fetch(
+          `https://api.giphy.com/v1/gifs/search?api_key=${giphyKey}&q=${query}&limit=10&offset=0&rating=PG-13&lang=en`
+        );
+        const json = await res.json();
+        console.log({ json });
+        setRestults(
+          json.data.map(item =>{
+            return item.images.preview.mp4;
+          })
+        );
+      } finally {
+        setLoading(false);
+      }
+    }
+    if (query !== '') {
+      fetchData();
+    }
+  }, [query]);
+  return [results, loading];
+}
+
+export default function AsyncHooks() {
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+  const [results, loading] = useGiphy(query);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>Async React Hooks</h1>
+      <form onSubmit={e => {
+        e.preventDefault();
+        setQuery(search);
+        }}
         >
-          Learn React
-        </a>
-      </header>
+        <input
+          value={search}
+          onChange={e => setSearch( e.target.value ) }
+          placeholder="Search"
+        />
+        <button type="submit">Search</button>
+      </form>
+      <br />
+      {loading ? (
+        <h1>Give me GIFS!</h1>
+      ) : (
+        results.map(item => <video autoPlay loop key={item} src={item}/>)
+      )}
     </div>
   );
 }
-
-export default App;
